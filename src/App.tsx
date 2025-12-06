d6237d37345555f5eda70ba20dd6fae9c28faff2
RAnami156 <romaandreev414@gmail.com>
2025-12-06 13:10:42 +0100

    todos blur

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,37 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';
+import { Todo } from './model';
 
 //красивая стрелочная функция главная
 const App: React.FC = () => {
 
   // создаем переменную в поторой есть еще функция setTodo и тип у нее стринг который мы указываем через <string>
   const [todo, setTodo] = useState<string>("");
+  const[todos, setTodos] = useState<Todo[]>([]) //массив
 
-  console.log(todo);
+  // функция
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo) {
+      setTodos([...todos, { id:Date.now(), todo, isDone: false}])
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
 
   // хахах тут наш любимый хтмлчик(я рофлю) и еще в реакте не юзаем class а юзаем className
   return (
     <div className="App">
       <span className="heading">Taskify</span>
       {/* здесь уже по трудне так как тут мы передаем данные элемент ребенку(тут мы в файле бати)   */}
-      <InputFeild todo={todo} setTodo={setTodo}/>
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      {/* <TodoList/> */}
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 }
--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './style.css';
 
 //присвоил типы просто, хули я коментирую это даже
 interface Props{
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void; //ничего не возвращает функция
 }
 
 // мейн функция которая получает переменную и функцию от бати   
-const InputFeild = ({todo, setTodo}: Props) => {
+const InputFeild = ({todo, setTodo, handleAdd}: Props) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+
+
     // тут опять хтмл начался
   return (
-    <form className='input'>
-      <input type="input" 
+    <form className='input' onSubmit={(e)=>{
+        handleAdd(e)
+        inputRef.current?.blur();
+        }}>
+
+        <input 
+        ref={inputRef}
+        type="input" 
     //   показываем в инпуте всегда то что есть в todo
         value={todo}
         // меняет setTodo в бате и заставляет его работать
